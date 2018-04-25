4aa0ff68f9e28e02c73a655d1520c14ed47a0c38
Derek Chen <derek.g.chen@gmail.com>
2018-04-25 21:20:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,4 +14,24 @@ class Book
    @author = author
  end
 
+ def author
+   @author
+ end
+
+ def page_count=(num)
+   @page_count = num
+ end
+
+ def page_count
+   @page_count
+ end
+
+ def genre=(genre)
+   @genre = genre
+ end
+
+ def genre
+   @genre
+ end
+
 end

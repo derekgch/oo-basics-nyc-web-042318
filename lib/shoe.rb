2daad183105ee244476f5386c0fe8c77211a3173
Derek Chen <derek.g.chen@gmail.com>
2018-04-25 21:27:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,11 @@ class Shoe
   attr_reader :brand, :color, :size, :material
   attr_accessor :condition
 
+  def initialize(brand)
+    @brand = brand
 
+  end
+  
   def cobble
 
     puts "new again"
